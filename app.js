959b87395a29b8f83e169c392e877781c1a6cb02
Rolando Cosme <rolandocosme@nbp-49-236.nbp.ruw.rutgers.edu>
2016-02-08 21:23:04 -0500

    working on handlebars syntax

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 var express = require('express');
-var handlebars = require('handlebars-express');
+var handlebars = require('express-handlebars');
 var app = express();
+
+var PORT = 8080;
+
+app.listen(PORT, function (){
+  console.log("listening");
+})
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+app.listen(PORT);
+
+app.get('/icecream', function (req, res){
+  var data = {
+    names: icecreams.name,
+    price: icecreams.price,
+    awesomeness: icecreams.awesomeness
+  }
+  res.render('main', data);
+});
